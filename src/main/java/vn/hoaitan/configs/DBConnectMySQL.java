f668dcc1cfa6c3477e8d609f7113fbbe62097b45
nguyenhoaitan <nguyenhoaitan31082004@gmail.com>
2024-09-18 13:56:19 +0700

    Tạo Project và kết nối với MySQL

---
+++ src/main/java/vn/hoaitan/configs/DBConnectMySQL.java
@@ -0,0 +1,76 @@
+package vn.hoaitan.configs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnectMySQL {
+    private static String USERNAME = "root";
+
+
+    private static String PASSWORD = "admin123";
+
+
+    private static String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+
+    private static String URL = "jdbc:mysql://localhost:3307/LapTrinhWeb";
+
+
+
+    public static Connection getDatabaseConnection() throws SQLException {
+
+
+        try {
+
+
+            Class.forName(DRIVER);
+
+
+        } catch (ClassNotFoundException e) {
+
+
+            // TODO Auto-generated catch block
+
+
+            e.printStackTrace();
+
+
+        }
+
+
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+
+    }
+
+
+
+    //Test chương trình. Kích phải chuột chọn run as->java application
+
+
+    public static void main(String[] args) {
+
+
+        try {
+
+
+            new DBConnectMySQL();
+
+
+            System.out.println(DBConnectMySQL.getDatabaseConnection());
+
+
+        }catch(Exception e) {
+
+
+            e.printStackTrace();
+
+
+        }
+
+
+    }
+
+
+}
